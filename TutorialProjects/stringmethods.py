1b48ff2e7753829234a74d4cc86cba7694b6f329
dspucch <dom.spucches@gmail.com>
2021-02-07 14:27:54 -0500

    update

---
+++ TutorialProjects/stringmethods.py
@@ -0,0 +1,12 @@
+course = 'Python for beginners'
+print(len(course))
+#len function prints length of the variable
+print(course.upper())#converts string to uppercase
+print(course.lower())#converts string to lowercase
+print(course.find('o'))#prints index of character
+print(course.find('beginners'))
+print(course.replace('beginners', 'Absolute Beginners')) #replaces first string with second string
+#^^ can also replace single characters
+print('Python' in course) #validating if "python" is in the variable
+#in operator produces a boolean value
+
