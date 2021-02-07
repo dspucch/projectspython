d14a2c59086e33a104a1e7bb9e9a8d03800f3cc0
dspucch <dom.spucches@gmail.com>
2021-02-07 14:40:07 -0500

    update

---
+++ TutorialProjects/integer2.py
@@ -0,0 +1,7 @@
+x = (10 + 3) * 2 ** 2
+print(x)
+#parenthese takes priority
+#exponentiation 2 ** 3
+#multiplication or division
+#addition or subtraction
+#order of operator
---
+++ TutorialProjects/integers.py
@@ -0,0 +1,10 @@
+print(10 % 3)
+#moduls % - returns remainder of the division
+#/ division
+# ** power of a number
+# * multiplication
+
+x = 10
+x = x + 3
+x += 3 #augmented assignment operator, does the above function
+print(x)
