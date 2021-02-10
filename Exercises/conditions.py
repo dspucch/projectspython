e95c4c4fc7edc076f471314536ccfb56629d60bf
dspucch <dom.spucches@gmail.com>
2021-02-09 21:35:00 -0500

    updated conditions(1)

--- Exercises/conditions.py
+++ Exercises/conditions.py
@@ -1,11 +1,10 @@
-good_credit = True
-house = 1000000
+house = float(1000000.00)
 good: float = float(.1)
 bad = float(.2)
-
+good_credit = True
 
 if good_credit:
-    print((house * good) +  "is required for down payment.")
+    print((house * good), "is required for down payment.")
 else:
-    print((house * bad) + ' is required for down payment.')
+    print((house * bad), ' is required for down payment.')
 
